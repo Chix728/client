b8c5288566ac9059873243b4c5765add5b6fb378
ChiragMaxlink <chirag.parmar@sayalife.in>
2022-01-02 19:28:50 +0530

    Completed Tutorial Youtube

---
+++ src/portfolioContainer/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from './profile'
+import Footer from './footer/footer'
+
+export default function Home(){
+    return (
+    <div className="home-container">
+        <Profile />
+        <Footer />
+    </div>
+    )
+}
